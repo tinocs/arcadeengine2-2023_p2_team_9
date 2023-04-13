ef5a3f0378f7771b9e558038a0d69e24a0c690a6
Aarav Daga <adaga460@student.fuhsd.org>
2023-04-13 09:58:21 -0700

    Update World.java

--- src/engine/World.java
+++ src/engine/World.java
@@ -1,13 +1,24 @@
 package engine;
 
+import javafx.animation.AnimationTimer;
 import javafx.scene.layout.Pane;
 
 // TODO finish the shit that goes in the code my dudes
 
 public abstract class World extends Pane {
-
-	public World() {
+	// private fields as listed
+	private Timer timer;
+	private boolean isOn = false;
+	
+	private double width = 0.0;
+	private double height = 0.0;
+	// methods
+	public World(Double w, double h) {
+		timer = new Timer();
 		
+		// lol i used this shitty way to make it just to get some errors out
+		width = width + w;
+		height = height + h;
 	}
 	
 	public abstract void act(long now);
@@ -26,7 +37,7 @@ public abstract class World extends Pane {
 		
 	}
 	public boolean isStopped() {
-		return false;
+		return isOn;
 		
 	}
 	public abstract void onDimensionsInitialized();
@@ -35,9 +46,20 @@ public abstract class World extends Pane {
 		
 	}
 	public void start() {
-		
+		timer.start();
+		isOn = true;
 	}
 	public void stop() { 
+		timer.stop();
+		isOn = false;
+	}
+	
+	private class Timer extends AnimationTimer {
+
+		@Override
+		public void handle(long now) {
+			act(now);
+		}
 		
 	}
 }
