81ba35b40bc71e96b66ea4b21a64fdacb34659ed
Aarav Daga <adaga460@student.fuhsd.org>
2023-04-13 09:17:04 -0700

    actor

--- src/engine/Actor.java
+++ src/engine/Actor.java
@@ -2,10 +2,36 @@ package engine;
 
 import javafx.scene.image.ImageView;
 
+//TODO finish the shit that goes in the code my dudes
+
 public abstract class Actor extends ImageView {
 
 	public Actor() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public abstract void act(long now);
+	
+	public void addedToWorld() {
+		
+	}
+	public double getHeight() {
+		return 0.0;
+	}
+	public double getWidth() {
+		return 0.0;
+	}
+	public <A extends Actor> void getIntersectingObjects(java.lang.Class<A> cls) {
+		
+	}
+	public <A extends Actor> void getOneIntersectingObject(java.lang.Class<A> cls) {
+		
+	}
+	public World getWorld() {
+		return null;
+	}
+	public void move(double dx, double dy) {
+		
 	}
 
 }
--- src/engine/World.java
+++ src/engine/World.java
@@ -2,9 +2,13 @@ package engine;
 
 import javafx.scene.layout.Pane;
 
+// TODO finish the shit that goes in the code my dudes
+
 public abstract class World extends Pane {
 
-	public World() {}
+	public World() {
+		
+	}
 	
 	public abstract void act(long now);
 	
@@ -12,6 +16,28 @@ public abstract class World extends Pane {
 		
 	}
 	public <A extends Actor> void getObjects(java.lang.Class<A> cls) {
+		
+	}
+	public <A extends Actor> void getObjectsAt(double x, double y, java.lang.Class<A> cls) {
+		
+	}
+	public boolean isKeyPressed(javafx.scene.input.KeyCode code) {
+		return false;
+		
+	}
+	public boolean isStopped() {
+		return false;
+		
+	}
+	public abstract void onDimensionsInitialized();
+	
+	public void remove(Actor actor) {
+		
+	}
+	public void start() {
+		
+	}
+	public void stop() { 
 		
 	}
 }
