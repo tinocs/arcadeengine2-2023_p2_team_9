b3a9be98f86f6276cb25ccfb3398e45e5c3980e1
Aarav Daga <adaga460@student.fuhsd.org>
2023-04-24 09:15:50 -0700

    Update World.java

--- src/engine/World.java
+++ src/engine/World.java
@@ -2,11 +2,15 @@ package engine;
 
 import java.awt.RenderingHints.Key;
 import java.util.ArrayList;
-
 import javafx.animation.AnimationTimer;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 
-// TODO finish the shit that goes in the code my dudes
+// TODO 
 
 public abstract class World extends Pane {
 	// private fields as listed
@@ -15,6 +19,7 @@ public abstract class World extends Pane {
 	
 	private boolean widthSet = false;
 	private boolean heightSet = false;
+	private boolean dimSet = false;
 	private double width = 0.0;
 	private double height = 0.0;
 	private ArrayList<Key> keysPressed;
@@ -24,6 +29,15 @@ public abstract class World extends Pane {
 		
 		keysPressed = new ArrayList<Key>();
 		
+		DoubleProperty widthListener = new SimpleDoubleProperty(width);
+		DoubleProperty heightListener = new SimpleDoubleProperty(height);
+		widthListener.addListener(new WidthHandler());
+		heightListener.addListener(new HeightHandler());
+		
+		this.sceneProperty().addListener(new SceneListener());
+		
+		//ArrayListProperty<Object> arrayList = new ArrayListProperty<>(); 
+		//arrayList.addListener();
 	}
 	
 	public abstract void act(long now);
@@ -31,7 +45,8 @@ public abstract class World extends Pane {
 	public abstract void onDimensionsInitialized();
 	
 	public void addActor(Actor actor) {
-		
+		this.getChildren().add(actor);
+		actor.addedToWorld();
 	}
 	public <A extends Actor> void getObjects(java.lang.Class<A> cls) {
 		
@@ -47,7 +62,7 @@ public abstract class World extends Pane {
 	}
 	
 	public void remove(Actor actor) {
-		
+		this.getChildren().remove(actor);
 	}
 	public void start() {
 		timer.start();
@@ -63,6 +78,29 @@ public abstract class World extends Pane {
 		@Override
 		public void handle(long now) {
 			act(now);
+		}
+		
+	}
+	
+	private class WidthHandler implements ChangeListener<Number> {
+		@Override
+		public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+			if (!widthSet) widthSet = true;
+			if (widthSet && heightSet && !dimSet) { onDimensionsInitialized(); dimSet = true; }
+		}
+    }
+	private class HeightHandler implements ChangeListener<Number> {
+		@Override
+		public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+			if (!heightSet) heightSet = true;
+			if (widthSet && heightSet && !dimSet) { onDimensionsInitialized(); dimSet = true; }
+		}
+    }
+	private class SceneListener implements ChangeListener<Scene> {
+
+		@Override
+		public void changed(ObservableValue<? extends Scene> observable, Scene oldValue, Scene newValue) {
+			
 		}
 		
 	}
