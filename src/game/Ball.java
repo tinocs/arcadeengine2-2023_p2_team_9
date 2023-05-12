0f8a9f3f0f32fb096771d422cc9e58360dbb82c7
Aryan Ajit <aryan.ajit645@gmail.com>
2023-05-12 11:49:00 -0700

    added ball

---
+++ src/game/Ball.java
@@ -0,0 +1,20 @@
+/**
+ * Aryan Ajit
+ * Period 2
+ * May 12, 2023
+ *
+ * Is this code Working:
+ */
+package game;
+
+public class Ball {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
