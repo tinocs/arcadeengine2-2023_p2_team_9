9d2a08ef5da8ad5cb7594c9c2c4952bd5e2c9757
Aarav Daga <adaga460@student.fuhsd.org>
2023-05-12 11:43:12 -0700

    Update Paddle.java

--- src/game/Paddle.java
+++ src/game/Paddle.java
@@ -5,8 +5,8 @@ import javafx.scene.image.Image;
 
 public class Paddle extends Actor {
 	
-	private static final String IMG_PREFIX = "testresources/";
-	private static final Image STAND_LEFT_IMG = new Image(IMG_PREFIX +"CharacterLeft_Standing.png");
+	private static final String IMG_PREFIX = "gameresources/";
+	private static final Image STAND_LEFT_IMG = new Image(IMG_PREFIX +"paddle.png");
 	
 	public Paddle() {
 		setImage(STAND_LEFT_IMG);
