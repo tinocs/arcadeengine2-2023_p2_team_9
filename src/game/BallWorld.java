b88218fad9bebc8237f8dc30cf7924e50afad1e7
Aryan Ajit <aryan.ajit645@gmail.com>
2023-05-15 12:45:09 -0700

    made mouse handler for paddle

--- src/game/BallWorld.java
+++ src/game/BallWorld.java
@@ -1,9 +1,13 @@
 package game;
 
+
+
 import engine.World;
-
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 public class BallWorld extends World {
-
+	Ball ball;
+	Paddle pad;
 	public BallWorld() {
 		setPrefSize(500, 500);
 	}
@@ -14,10 +18,25 @@ public class BallWorld extends World {
 
 	@Override
 	public void onDimensionsInitialized() {
-		Ball ball = new Ball();
-		ball.setX(40);
-		ball.setY(60);
+		ball = new Ball();
+		ball.setX(250);
+		ball.setY(250);
 		add(ball);
+		
+		pad = new Paddle();
+		pad.setX(460);
+		pad.setY(460);
+		add(pad);
+		
+		setOnMouseMoved(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				pad.setX(event.getX()- pad.getWidth()/2);
+			}
+			
+		});
 	}
 
 }
