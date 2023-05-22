4684f892a99d254d5eb570622cc1c36d94ad5904
Aarav Daga <adaga460@student.fuhsd.org>
2023-05-22 10:18:44 -0700

    Update GameStart.java

--- src/mario/GameStart.java
+++ src/mario/GameStart.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import tests.TestWorld;
 
 public class GameStart extends Application {
 
