f87aab26deac5d3c870c282b2ce4630ea3146fbb
Aarav Daga <adaga460@student.fuhsd.org>
2023-05-23 09:32:04 -0700

    walking changes and world changes (read desc)
    - mario now walks normally, animation works
    - world now moves as mario moves
    - added some getter methods in MarioPlayer.java
    - added moveAll method which moves every object other than mario in the world class.

--- src/mario/MarioPlayer.java
+++ src/mario/MarioPlayer.java
@@ -14,24 +14,25 @@ import javafx.scene.input.KeyCode;
 public class MarioPlayer extends Actor{
 	double speed;
 	boolean canJump = false;
-	int vel;
+	int vel = 0;
 	int jumpSpeed = 4;
-	
+	int frame = 1;
+	boolean isRight = false;
 	//images
 	private static final String IMG_PREFIX = "gameresources/";
     	//right
-	    private static final Image jump1 = new Image(IMG_PREFIX +"jump1.png");
-	    private static final Image runR1 = new Image(IMG_PREFIX +"runR1.png");
-	    private static final Image runR2 = new Image(IMG_PREFIX +"runR2.png");
-	    private static final Image runR3 = new Image(IMG_PREFIX +"runR3.png");
-	    private static final Image stand1 = new Image(IMG_PREFIX +"stand1.png");
+	    private static final Image jump1 = new Image(IMG_PREFIX +"jump1.png", 20, 30, false, false);
+	    private static final Image runR1 = new Image(IMG_PREFIX +"runR1.png", 20, 30, false, false);
+	    private static final Image runR2 = new Image(IMG_PREFIX +"runR2.png", 20, 30, false, false);
+	    private static final Image runR3 = new Image(IMG_PREFIX +"runR3.png", 20, 30, false, false);
+	    private static final Image stand1 = new Image(IMG_PREFIX +"stand1.png", 20, 30, false, false);
     	//left
-	    private static final Image jump2 = new Image(IMG_PREFIX +"jump2.png");
-	    private static final Image runL1 = new Image(IMG_PREFIX +"runL1.png");
-	    private static final Image runL2 = new Image(IMG_PREFIX +"runL2.png");
-	    private static final Image runL3 = new Image(IMG_PREFIX +"runL3.png");
-	    private static final Image stand2 = new Image(IMG_PREFIX +"stand2.png");
-    
+	    private static final Image jump2 = new Image(IMG_PREFIX +"jump2.png", 20, 30, false, false);
+	    private static final Image runL1 = new Image(IMG_PREFIX +"runL1.png", 20, 30, false, false);
+	    private static final Image runL2 = new Image(IMG_PREFIX +"runL2.png", 20, 30, false, false);
+	    private static final Image runL3 = new Image(IMG_PREFIX +"runL3.png", 20, 30, false, false);
+	    private static final Image stand2 = new Image(IMG_PREFIX +"stand2.png", 20, 30, false, false);
+	    
     
     public MarioPlayer() {
     	setImage(stand1);
@@ -46,38 +47,51 @@ public class MarioPlayer extends Actor{
 	}
 	
 	private void controls() {
-		if(getWorld().isKeyPressed(KeyCode.LEFT) || getWorld().isKeyPressed(KeyCode.A)) {
-			int frame = 1;
-			move(-speed,0);
-			if(frame == 1) {
+		
+		 if((getWorld().isKeyPressed(KeyCode.UP) || getWorld().isKeyPressed(KeyCode.W)) && getOneIntersectingObject(Brick.class) != null) {
+				jump();
+		 } else if ((getWorld().isKeyPressed(KeyCode.LEFT) || getWorld().isKeyPressed(KeyCode.A)) ) {
+			if (getX() > 100) 
+				move(-speed,0);
+			if(frame % 6 == 0) {
 				setImage(runL1);
-				frame++;
-			}else if(frame == 2) {
+			}else if(frame % 4 == 0) {
 				setImage(runL2);
-				frame++;
-			}else if(frame == 3) {
+			}else if (frame % 2 == 0){
 				setImage(runL3);
-				frame=1;
 			}
-		}
-		if(getWorld().isKeyPressed(KeyCode.RIGHT) || getWorld().isKeyPressed(KeyCode.D)) {
-			int frame = 1;
-			move(speed,0);
-			if(frame == 1) {
+			frame++;
+			isRight = false;
+		} else if ((getWorld().isKeyPressed(KeyCode.RIGHT) || getWorld().isKeyPressed(KeyCode.D))) {
+			if (getX() < getWorld().getWidth() - 100)
+				move(speed,0);
+			if(frame % 6 == 0) {
 				setImage(runR1);
-				frame++;
-			}else if(frame == 2) {
+			}else if(frame % 4 == 0) {
 				setImage(runR2);
-				frame++;
-			}else if(frame == 3) {
+			}else if (frame % 2 == 0){
 				setImage(runR3);
-				frame=1;
 			}
-			System.out.println("dan");
+			frame++;
+			isRight = true;
+			//System.out.println("dan");
+		} else {
+			if (isRight) {
+				setImage(stand1);
+			} else {
+				setImage(stand2);
+			}
+			//frame = 1;
 		}
-		if(getWorld().isKeyPressed(KeyCode.UP) || getWorld().isKeyPressed(KeyCode.W)) {
-			jump();
-		}
+	}
+	public boolean isGoingRight() {
+		return (getWorld().isKeyPressed(KeyCode.RIGHT) || getWorld().isKeyPressed(KeyCode.D));
+	}
+	public boolean isGoingLeft() {
+		return (getWorld().isKeyPressed(KeyCode.LEFT) || getWorld().isKeyPressed(KeyCode.A));
+	}
+	public double getSpeed() {
+		return speed;
 	}
 	private void gravity() {
 		// TODO Auto-generated method stub
@@ -96,7 +110,7 @@ public class MarioPlayer extends Actor{
 	private void jump() {
 		// TODO Auto-generated method stub
 		if(canJump) {
-			vel = -25;
+			vel = -15;
 			setY(getY() - .5);
 		}
 	}
--- src/mario/MarioWorld.java
+++ src/mario/MarioWorld.java
@@ -1,16 +1,24 @@
 package mario;
 
+import engine.Actor;
 import engine.World;
+import javafx.scene.Node;
 
 public class MarioWorld extends World {
-
+	MarioPlayer mario;
+	int w = 870;
+	int h = 500;
 	public MarioWorld() {
-		setPrefSize(870, 500);
+		setPrefSize(w, h);
 	}
 
 	@Override
 	public void act(long now) {
-		
+		if (mario.getX() > w - 150 && mario.isGoingRight()) {
+			moveAll((int)-mario.getSpeed(), 0);
+		} else if (mario.getX() < 150 && mario.isGoingLeft()) {
+			moveAll((int)mario.getSpeed(), 0);
+		}
 	}
 	
 	@Override
@@ -20,15 +28,15 @@ public class MarioWorld extends World {
 		int y = (int) (getHeight()-new Brick().getHeight());
 		makeBottomBricks(x, y);
 		
-		MarioPlayer mario = new MarioPlayer();
+		mario = new MarioPlayer();
 		mario.setX(40);
-		mario.setY(40);
+		mario.setY(y - new Brick().getHeight()*4 - mario.getHeight());
 		add(mario);
 	}
 	
 	public void makeBottomBricks(int x, int y) {
 		for (int j = 0; j < 4; j++) {
-			for (int i = 0; i < 32; i++) {
+			for (int i = 0; i < 100; i++) {
 				Brick b = new Brick();
 				b.setX(x);
 				b.setY(y+2*j);
@@ -37,6 +45,14 @@ public class MarioWorld extends World {
 			}
 			y -= new Brick().getHeight();
 			x = 0;
+		}
+	}
+	
+	public void moveAll(int xDir, int yDir) {
+		for (Node n : getChildren()) {
+			if (n != mario) {
+				((Actor) n).move(xDir, yDir);
+			}
 		}
 	}
 
