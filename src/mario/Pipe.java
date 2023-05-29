6281d71f45da8459975439b016932b40fc62d545
Aryan Ajit <aryan.ajit645@gmail.com>
2023-05-28 21:30:47 -0700

    some update to pipe

--- src/mario/Pipe.java
+++ src/mario/Pipe.java
@@ -10,18 +10,32 @@ package mario;
 import javafx.scene.image.Image;
 
 public class Pipe extends Block{
+	int height;
+	int dir;
 	//images
 		private static final String IMG_PREFIX = "gameresources/";
-		private static final Image img  = new Image(IMG_PREFIX + "topPipe.png");
+		private static final Image top  = new Image(IMG_PREFIX + "topPipe.png");
+		private static final Image left  = new Image(IMG_PREFIX + "leftpipe.png");
+		private static final Image right  = new Image(IMG_PREFIX + "rightPipe.png");
 	
-	public Pipe() {
-		setImage(img);
+	public Pipe(int height, int dir) {
+		this.height = height;
+		this.dir = dir;
+		
+		if(dir == 1) {
+			setImage(top);
+		}else if(dir == 2) {
+			setImage(right);
+		}else {
+			setImage(left);
+		}
 		getTimer().start();
 	}
 	
 	@Override
 	public void act(long now) {
 		// TODO Auto-generated method stub
+		
 	}
 
 }
